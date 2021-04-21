2fdea0f6578265597523000650f6742b94510200
Li-ping <wlts80127@gmail.com>
2021-04-21 09:21:17 +0800

    nav debug

--- 網站/JS/nav.js
+++ 網站/JS/nav.js
@@ -1,9 +1,11 @@
 document.querySelector('.buttonarea button').addEventListener('click',e=>{
     if(document.querySelector('#aaa').style.height != '60px'){
         document.querySelector('#aaa').style.height = 60 + 'px';
+        document.querySelector('nav').classList.add('heightchange');
     }
     else{
         document.querySelector('#aaa').style.height = '';
+        document.querySelector('nav').classList.remove('heightchange');
     }
 })
 
