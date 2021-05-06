37c606c73dcbada3807a01436f783ce1e670ffcf
Li-ping <wlts80127@gmail.com>
2021-05-06 09:14:38 +0800

    footer and nav

--- 網站/JS/nav.js
+++ 網站/JS/nav.js
@@ -1,16 +1,16 @@
 document.querySelector('.buttonarea button').addEventListener('click',e=>{
-    if(document.querySelector('#aaa').style.height != '60px'){
-        document.querySelector('#aaa').style.height = 60 + 'px';
+    if(document.querySelector('#listItem').style.height != '60px'){
+        document.querySelector('#listItem').style.height = 60 + 'px';
         document.querySelector('nav').classList.add('heightchange');
     }
     else{
-        document.querySelector('#aaa').style.height = '';
+        document.querySelector('#listItem').style.height = '';
         document.querySelector('nav').classList.remove('heightchange');
     }
 })
 
 window.addEventListener('resize',e=>{
     if(window.innerWidth>=576){
-        document.querySelector('#aaa').style.height = '';
+        document.querySelector('#listItem').style.height = '';
     }
 });
