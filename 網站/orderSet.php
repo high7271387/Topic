4f3a106a1dbcee762fcbea2c6b3abb608fd9b1cc
Wei08705110 <81276876+aa08705110@users.noreply.github.com>
2021-05-11 16:17:28 +0800

    Revert "Updata"
    This reverts commit 10a72ac897aa56e33a98882db18b10029302e7b3.

--- 網站/orderSet.php
+++ 網站/orderSet.php
@@ -1,6 +1,6 @@
 <?php
     require_once('dbconnect.php');
-    // require_once('strFilter.php');
+    require_once('strFilter.php');
     header("Content-Type:text/html; charset=utf-8");
     $data = json_decode(file_get_contents("php://input"));
     if($data==''){
@@ -13,7 +13,7 @@
         $orderSn = date('Ymd') . str_pad(mt_rand(1, 99999), 5, '0', STR_PAD_LEFT);
         foreach($data as $item){
             $str = $item -> remark;
-            // $str = strFilter($str);
+            $str = strFilter($str);
             $sql = "INSERT INTO `orderdata` (`tracking_number`,`username`, `p_name`, `p_number`,`p_color`,`p_price`, `p_remark`, `date`) VALUES ('$orderSn','$item->username','$item->product','$item->number','$item->color','$item->price','$str','$date') ";
             $result = mysqli_query($connect , $sql);
             if(mysqli_affected_rows($connect) <= 0){
