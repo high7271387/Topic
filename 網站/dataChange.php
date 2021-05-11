3d17df76ed88988571407df26e1299d5540cb382
Li-ping <wlts80127@gmail.com>
2021-05-11 13:08:35 +0800

    Update dataChange.php
    修正此頁面的樣式

--- 網站/dataChange.php
+++ 網站/dataChange.php
@@ -1,30 +1,149 @@
 <?php
     require_once('dbconnect.php');
-
-    if(isset($_REQUEST['username'])){
-        $username = $_REQUEST['username'];
-        $name = $_REQUEST['name'];
-        $phone = $_REQUEST['phone'];
-        $idNumber = $_REQUEST['idNumber'];
-        $email = $_REQUEST['email'];
-        $address = $_REQUEST['address'];
-    
-        $sql = "UPDATE `user` SET `name` = '$name', `phone`= '$phone' , `idNumber` = '$idNumber' , `email` = '$email' , `address`='$address'  WHERE `username` = '$username' ";
-        $result = mysqli_query($connect,$sql);
-        if(mysqli_affected_rows($connect) > 0){
-            echo "資料更新成功";
-        }
-        elseif(mysqli_affected_rows($connect) == 0){
-            echo "資料更新失敗";
-        }
-        else{
-            echo  "{$sql} 語法執行失敗，錯誤訊息：" ;
-        }
-        mysqli_close($connect);
-    }
-    else{
-        header("Location: index.html");
-    }
-
-
 ?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>登出</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/fontset.css">
+    <link rel="stylesheet" href="css/nav.css">
+    <link rel="stylesheet" href="CSS/footer.css">
+    <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=false"></script> 
+</head>
+    <style>
+        body{
+            background-color: black;
+            
+        }
+        main{
+            display: grid;
+            grid-template-rows: 120px 60px 1fr;
+            grid-template-columns: 20vw 60vw 20vw;
+            background-color: #000;
+            padding-bottom: 40px;
+        }
+        main h1{
+            grid-row: 2/3;
+            grid-column: 2/3;
+            font-size: 1.9rem;
+            font-weight: bold;
+            color:white;
+            padding-left: 5px;
+            position: relative;
+        }
+        main h1::after{
+            content: '';
+            position: absolute;
+            border-bottom: 3px red solid;
+            width:100%;
+            left:0px;
+            bottom: 20px;
+        }
+        main .content{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            border: 3px solid rgb(235, 235, 235);
+            border-radius: 10px ;
+            grid-row: 3/4;
+            grid-column: 2/3;
+            padding:2vh 5vw 4vh 5vw;
+            color:white;
+            height: 300px;
+            font-size: 1.6rem;
+            margin-bottom: 40px;
+        }
+        main .content button{
+            position: absolute;
+            border: 1px solid rgb(177, 177, 177);
+            border-radius: 10px;
+            background-color: #000;
+            color:azure;
+            bottom:-50px;
+            right:20px;
+            padding: 5px 20px;
+            font-weight: bold;
+            cursor: pointer;
+            transition: all 0.4s;
+        }
+        main .content button:hover{
+            background-color: rgb(189, 189, 189);
+            color:black
+        }
+        main .content button:active{
+            background-color: rgb(90, 90, 90);
+            color:rgb(226, 226, 226)       
+        }
+
+        footer{
+            text-align: center;
+            background: #000;
+            color: white;
+        }
+    </style>
+<body>
+<nav>
+        <div class="logo"><a href="index.html"> </a></div>
+        <div class='buttonarea'>
+            <button type='button'><i class="fas fa-align-justify fa-2x"></i></button>
+        </div>
+        <div class="navlist" id='listItem'>
+            <div class="product"> <a href="productList.html"></a> </div>
+            <div class="news"> <a href="news.html"></a> </div>
+            <div class="cart"> <a href="shoppingCart.html"></a></div>
+            <div class="member"><a href="userdata.html"></a></div>
+        </div>
+    </nav>
+
+    <main>
+        <div class="content">
+            <?php
+                    if(isset($_REQUEST['username'])){
+                        $username = $_REQUEST['username'];
+                        $name = $_REQUEST['name'];
+                        $phone = $_REQUEST['phone'];
+                        $idNumber = $_REQUEST['idNumber'];
+                        $email = $_REQUEST['email'];
+                        $address = $_REQUEST['address'];
+                    
+                        $sql = "UPDATE `user` SET `name` = '$name', `phone`= '$phone' , `idNumber` = '$idNumber' , `email` = '$email' , `address`='$address'  WHERE `username` = '$username' ";
+                        $result = mysqli_query($connect,$sql);
+                        if(mysqli_affected_rows($connect) > 0){
+                            header("Refresh: 2; url=modify.html");
+                            echo "資料更新成功";
+                        }
+                        elseif(mysqli_affected_rows($connect) == 0){
+                            header("Refresh: 2; url=modify.html");
+                            echo "資料更新失敗";
+                        }
+                        else{
+                            echo  "{$sql} 語法執行失敗，錯誤訊息：" ;
+                        }
+                        mysqli_close($connect);
+                    }
+                    else{
+                        header("Location: index.html");
+                    }
+
+            ?>
+        </div>
+        
+    </main>
+
+    <footer>
+        Copyright © 2021 109年電子商務網站建置(臺中)第2期第八組. All rights reserved.
+    </footer>
+
+    <script src='js/nav.js'></script>
+
+</body>
+</html>
