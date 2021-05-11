07d47e863d8e0b8af801a796c61e0ce72c983601
Li-ping <wlts80127@gmail.com>
2021-05-11 15:26:24 +0800

    Update changePassword.php

--- 網站/changePassword.php
+++ 網站/changePassword.php
@@ -1,16 +1,15 @@
-<?php 
-    require_once('dbconnect.php');
-    if(!isset($_COOKIE['username'])){
-        header("Location: login.html");
-    }
-    if(isset($_REQUEST['oldPassword'])){
-        $block1 = 'none';
-        $block2 = 'block';
-    }
-    else{
-        $block1 = 'block';
-        $block2 = 'none';
-    }
+<?php
+require_once('dbconnect.php');
+if (!isset($_COOKIE['username'])) {
+    header("Location: login.html");
+}
+if (isset($_REQUEST['oldPassword'])) {
+    $block1 = 'none';
+    $block2 = 'block';
+} else {
+    $block1 = 'block';
+    $block2 = 'none';
+}
 ?>
 
 <!DOCTYPE html>
@@ -21,22 +20,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/fontset.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="CSS/nav.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="CSS/footer.css">
+    <title>密碼修改</title>
     <style>
         body {
             margin: 0px;
         }
 
         #span {
-            height: 100px;
+            height: 80px;
             background-color: #000;
         }
 
         #content {
-            height:100vh;
+            height: 100vh;
             width: 100%;
             background-color: #000;
         }
@@ -49,21 +48,23 @@
             width: 60.5%;
             background-color: #000;
         }
-        #change h1{
+
+        #change h1 {
             grid-row: 2/3;
             grid-column: 2/3;
             font-size: 1.9rem;
             font-weight: bold;
-            color:white;
+            color: white;
             padding-left: 5px;
             position: relative;
         }
-        #change h1::after{
+
+        #change h1::after {
             content: '';
             position: absolute;
             border-bottom: 3px red solid;
-            width:100%;
-            left:0px;
+            width: 100%;
+            left: 0px;
             bottom: -10px;
         }
 
@@ -76,7 +77,7 @@
         .text {
             float: left;
             text-align: center;
-            width:20%;
+            width: 20%;
             height: 50px;
             color: white;
             font-size: 28px;
@@ -96,7 +97,7 @@
 
         .formitem {
             text-align: center;
-            display:flex;
+            display: flex;
             flex-wrap: wrap;
             justify-content: center;
         }
@@ -109,47 +110,54 @@
             font-weight: bold;
             border-radius: 29px;
         }
-        .formitem input[type='submit']{
+
+        .formitem input[type='submit'] {
             padding: 10px 15px;
             cursor: pointer;
         }
-        .formitem input[type='submit']:hover{
-            background-color:whitesmoke;
+
+        .formitem input[type='submit']:hover {
+            background-color: whitesmoke;
             color: black;
         }
 
         .formitem button:nth-child(2) {
             margin-left: 50px;
         }
-        .result p{
-            font-size:24px;
+
+        .result p {
+            font-size: 24px;
         }
-        .result a{
-            color:white;  
-            text-decoration:none; 
-            border:1px solid white; 
-            border-radius:5px; 
-            padding:5px 10px;
+
+        .result a {
+            color: white;
+            text-decoration: none;
+            border: 1px solid white;
+            border-radius: 5px;
+            padding: 5px 10px;
         }
-        .result a:hover{
-            color:black;  
-            background:#F5F5F5	;
+
+        .result a:hover {
+            color: black;
+            background: #F5F5F5;
         }
 
 
         @media screen and (max-width:1200px) {
             .text {
-                width:100px;
+                width: 100px;
                 font-size: 18px;
                 margin-top: 4px;
             }
         }
+
         @media screen and (max-width:731px) {
             .text {
                 width: 100%;
                 text-align: center;
                 height: 35px;
             }
+
             .inputtext {
                 width: 100%;
             }
@@ -177,16 +185,15 @@
 <body>
 
     <nav>
-        <div class="logo"><a href="#"> </a></div>
+        <div class="logo"><a href="index.html"> </a></div>
         <div class='buttonarea'>
             <button type='button'><i class="fas fa-align-justify fa-2x"></i></button>
         </div>
-        <div class="navlist" id='aaa'>
-            <div class="product">
-            </div>
-            <div class="news"></div>
-            <div class="about"></div>
-            <div class="address"></div>
+        <div class="navlist" id='listItem'>
+            <div class="product"> <a href="productList.html"></a> </div>
+            <div class="news"> <a href="news.html"></a> </div>
+            <div class="cart"> <a href="shoppingCart.html"></a></div>
+            <div class="member"><a href="userdata.html"></a></div>
         </div>
     </nav>
     <div id="span"></div>
@@ -194,7 +201,7 @@
         <div id="change">
             <form action="changePassword.php" method="POST">
                 <h1>密碼修改</h1>
-                <div id="changeContent"  style="display:<?php echo $block1;?>">
+                <div id="changeContent" style="display:<?php echo $block1; ?>">
                     <div class='formitem'>
                         <label for="oldPassword" class='text'>原密碼</label>
                         <input type="password" class="inputtext" id='oldPassword' name='oldPassword'>
@@ -207,65 +214,63 @@
                         <label for="password2" class='text'>新密碼確認</label>
                         <input type="password" class="inputtext" id='password2' name='password2'>
                     </div>
-                    <div class='formitem' >
+                    <div class='formitem'>
                         <input type="submit" value="確認修改"></input>
                     </div>
                 </div>
             </form>
-            <div class="result" style="display:<?php echo $block2;?>; color:white; text-align:center;">
-                <?php 
-                    
-                    $username = $_COOKIE['username'];
-                    $oldpassword = $_REQUEST['oldPassword'];
-                    $sql1 = "SELECT * FROM user WHERE username = '$username' AND password = '$oldpassword'";
-                    $result = mysqli_query($connect , $sql1);
-                    if(mysqli_num_rows($result)>0){
-                        $newpassword = $_REQUEST['password'];
-                        $sql2 = "UPDATE user SET  password= '$newpassword' WHERE username = '$username'";
-                        $result2 = mysqli_query($connect , $sql2);
-                        if(mysqli_affected_rows($connect)>0){
-                            echo '<p>密碼更新成功</p>';
-                        }
-                        else{
-                            echo '<p>更新失敗，請再試一次。</p>';
-                        }
+            <div class="result" style="display:<?php echo $block2; ?>; color:white; text-align:center;">
+                <?php
 
+                $username = $_COOKIE['username'];
+                $oldpassword = $_REQUEST['oldPassword'];
+                $sql1 = "SELECT * FROM user WHERE username = '$username' AND password = '$oldpassword'";
+                $result = mysqli_query($connect, $sql1);
+                if (mysqli_num_rows($result) > 0) {
+                    $newpassword = $_REQUEST['password'];
+                    $sql2 = "UPDATE user SET  password= '$newpassword' WHERE username = '$username'";
+                    $result2 = mysqli_query($connect, $sql2);
+                    if (mysqli_affected_rows($connect) > 0) {
+                        echo '<p>密碼更新成功</p>';
+                    } else {
+                        echo '<p>更新失敗，請再試一次。</p>';
                     }
-                    else{
-                        echo '<p>查不到對應的密碼資料，請確認您所輸入的密碼是否正確。</p>';
-                    }
-                    
+                } else {
+                    echo '<p>查不到對應的密碼資料，請確認您所輸入的密碼是否正確。</p>';
+                }
+
                 ?>
                 <br>
-                 <a href="userdata.html" >回會員中心</a>
+                <a href="userdata.html">回會員中心</a>
             </div>
         </div>
     </div>
-
+    <footer>
+        Copyright © 2021 109年電子商務網站建置(臺中)第2期第八組. All rights reserved.
+    </footer>
 </body>
 <script src='JS/nav.js'></script>
 
 <script>
     const passwordRules = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{5,}$/;
 
-    document.querySelector('input[type="submit"]').addEventListener('click',e=>{
-            if(!passwordRules.test(document.querySelector('#oldPassword').value)){
-                document.querySelector('#oldPassword').focus();
-                alert('舊密碼格式有誤，請輸入正確的格式');
-                e.preventDefault();
-                return;
-            }
-            if(!passwordRules.test(document.querySelector('#password').value)){
-                document.querySelector('#password').focus();
-                alert('新密碼格式有誤，請輸入正確的格式');
-                e.preventDefault();
-                return;
-            }
-            else if(document.querySelector('#password').value !== document.querySelector('#password2').value){
-                document.querySelector('#password2').focus();
-                alert('新密碼與確認密碼不一致，請再次確認');
-                e.preventDefault();
-                return;
+    document.querySelector('input[type="submit"]').addEventListener('click', e => {
+        if (!passwordRules.test(document.querySelector('#oldPassword').value)) {
+            document.querySelector('#oldPassword').focus();
+            alert('舊密碼格式有誤，請輸入正確的格式');
+            e.preventDefault();
+            return;
+        }
+        if (!passwordRules.test(document.querySelector('#password').value)) {
+            document.querySelector('#password').focus();
+            alert('新密碼格式有誤，請輸入正確的格式');
+            e.preventDefault();
+            return;
+        } else if (document.querySelector('#password').value !== document.querySelector('#password2').value) {
+            document.querySelector('#password2').focus();
+            alert('新密碼與確認密碼不一致，請再次確認');
+            e.preventDefault();
+            return;
         }
     });
 </script>
